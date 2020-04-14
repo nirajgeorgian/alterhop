b0888c190468a23ecbc2d797175d8a0649f7c9c7
nirajgeorgian <niraj.kishore@stayology.com>
2020-04-15 00:38:00 +0530

    setting up base structure #2

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ const App = () => (
 	<div className={styles.pageWrapper}>
 		<div className={styles.mainWrapper}>
 			<Row>
-				<Layout>
+				<Layout hasSider={true}>
 					<Col className={styles.mainMenu} xxl={6} xl={6} lg={8} md={8} xs={24} sm={24}>
 						<Sider width="100%" className={styles.navigation}>
 							navigation
 						</Sider>
 					</Col>
 					<Col xxl={18} xl={18} lg={16} md={16} xs={24} sm={24}>
-						<Layout>
+						<Layout hasSider={false}>
 							<Content>content</Content>
 						</Layout>
 					</Col>
