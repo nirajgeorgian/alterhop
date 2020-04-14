5b10d4aeb6007f71d3f3121a42fe6a6ff2c9a05c
nirajgeorgian <niraj.kishore@stayology.com>
2020-04-15 00:24:47 +0530

    setting up base structure #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Layout, Row, Col } from 'antd'
+import styles from './App.module.less'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const { Content, Sider } = Layout
 
-export default App;
+const App = () => (
+	<div className={styles.pageWrapper}>
+		<div className={styles.mainWrapper}>
+			<Row>
+				<Layout>
+					<Col className={styles.mainMenu} xxl={6} xl={6} lg={8} md={8} xs={24} sm={24}>
+						<Sider width="100%" className={styles.navigation}>
+							navigation
+						</Sider>
+					</Col>
+					<Col xxl={18} xl={18} lg={16} md={16} xs={24} sm={24}>
+						<Layout>
+							<Content>content</Content>
+						</Layout>
+					</Col>
+				</Layout>
+			</Row>
+		</div>
+	</div>
+)
+
+export default App
---
+++ typings/global.d.ts
@@ -0,0 +1,36 @@
+declare const process: any
+declare const require: any
+
+declare interface Window {
+	__REDUX_DEVTOOLS_EXTENSION__: any
+	__REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any
+}
+
+declare interface NodeModule {
+	hot?: { accept: (path: string, callback: () => void) => void }
+}
+
+declare interface System {
+	import<T = any>(module: string): Promise<T>
+}
+
+declare let System: System
+
+declare module '*.module.less' {
+	const classes: { [key: string]: string }
+	export default classes
+}
+
+declare module '*.less' {
+	interface IClassNames {
+		[className: string]: string
+	}
+	const classNames: IClassNames
+	export = classNames
+}
+
+declare module '*.svg' {
+	const content: string
+
+	export default content
+}
