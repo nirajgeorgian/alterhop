dc27d8f01c6ccb0bfed35a555505fef0c0360f24
nirajgeorgian <niraj.kishore@stayology.com>
2020-05-02 04:20:13 +0530

    Added ode for navigation

--- src/app/navigation/secondary/index.tsx
+++ src/app/navigation/secondary/index.tsx
@@ -1,4 +1,4 @@
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons'
+import { AppstoreOutlined, BookOutlined, MessageOutlined, UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons'
 
 import { Link } from 'react-router-dom'
 import { Menu } from 'antd'
@@ -8,14 +8,14 @@ import styles from 'app/navigation/style.module.less'
 export const RootMenu = (props) => (
 	<Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} className={styles.secondary}>
 		<Menu.Item key="1" className={styles.hovered}>
-			<Link to={`${props.location.pathname}/feeds`}>
+			<Link to='/jobs'>
 				<UserOutlined />
-				<span>Root Menu 1</span>
+				<span>Jobs</span>
 			</Link>
 		</Menu.Item>
 		<Menu.Item key="2" className={styles.hovered}>
 			<VideoCameraOutlined />
-			<span>Root Menu 2</span>
+			<span>Companies</span>
 		</Menu.Item>
 		<Menu.Item key="3" className={styles.hovered}>
 			<UploadOutlined />
@@ -24,11 +24,31 @@ export const RootMenu = (props) => (
 	</Menu>
 )
 
-export const ProfileMenu = (props) => (
+export const ProfileMenu = () => (
 	<Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} className={styles.secondary}>
-		<Menu.Item key="1" className={styles.hovered}>
-			<UserOutlined />
-			<Link to={`${props.location.pathname}/feed`}>profile</Link>
+		<Menu.Item key="1">
+			<Link to="/profile/username">
+				<UserOutlined />
+				<span>My Profile</span>
+			</Link>
+		</Menu.Item>
+		<Menu.Item key="2">
+			<AppstoreOutlined />
+			<span>Jobs</span>
+		</Menu.Item>
+		<Menu.Item key="3">
+			<BookOutlined />
+			<span>Saved Jobs</span>
+		</Menu.Item>
+		<Menu.Item key="4">
+			<Link to="/profile/message">
+				<MessageOutlined />
+				<span>Messages</span>
+			</Link>
+		</Menu.Item>
+		<Menu.Item key="5">
+			<VideoCameraOutlined />
+			<span>Interviews</span>
 		</Menu.Item>
 	</Menu>
 )
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,4 +1,4 @@
-import { CompanyMenu, RootMenu } from 'app/navigation'
+import { CompanyMenu, ProfileMenu, RootMenu } from 'app/navigation'
 import { Route, Switch } from 'react-router-dom'
 
 import Job from 'app/pages/job/page'
@@ -8,13 +8,13 @@ import Profile from 'app/pages/profile/page'
 import React from 'react'
 import Test from 'app/pages/test/page'
 import style from 'app/style.module.less'
-import ProfileSideBar from 'components/ProfileSideBar'
 
 export const PrimaryRoutes: React.FC = () =>
   <div className={style.routes}>
     <Switch>
-      <Route path="/profile" exact component={ProfileSideBar} />
+      <Route path="/profile" component={ProfileMenu} />
       <Route path="/company" component={CompanyMenu} />
+      <Route path="/" component={RootMenu} />
     </Switch>
   </div>
 
@@ -22,9 +22,10 @@ export const PrimaryRoutes: React.FC = () =>
 export const SecondaryRoutes: React.FC = () =>
   <div className={style.routes}>
     <Switch>
-      <Route path="/" exact component={Message} />
+      <Route path="/jobs" exact component={Job} />
       <Route path="/test" exact component={Test} />
-      <Route path="/profile" component={Profile} />
+      <Route path="/profile/message" exact component={Message} />
+      <Route path="/profile/username" exact component={Profile} />
       <Route path="/company/jobs" component={Job} />
       <Route path="/profile/payment" component={Payment} />
     </Switch>
